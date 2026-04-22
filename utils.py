27eb10d8b47e0e33be451de57d334192639e6e17
Piyush Taneja <tanejapiyush433@gmail.com>
2026-04-22 18:01:24 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,20 +1,23 @@
 import torch
 import matplotlib.pyplot as plt
+import config
 
 def sparsity_loss(model):
     loss = 0
+    count = 0
     for gates in model.get_all_gates():
         loss += torch.sum(gates)
-    return loss
+        count += gates.numel()
+    return 5 * (loss / count)
 
 
-def calculate_sparsity(model, threshold=1e-2):
+def calculate_sparsity(model):
     total = 0
     pruned = 0
 
     for gates in model.get_all_gates():
         total += gates.numel()
-        pruned += torch.sum(gates < threshold).item()
+        pruned += torch.sum(gates < config.THRESHOLD).item()
 
     return 100 * pruned / total
 
@@ -24,6 +27,7 @@ def plot_gate_distribution(model, path):
     for gates in model.get_all_gates():
         all_gates.extend(gates.detach().cpu().numpy().flatten())
 
+    plt.figure()
     plt.hist(all_gates, bins=50)
     plt.title("Gate Distribution")
     plt.savefig(path)
