14f0f8c7490cf8dfdbaba203a1a0207139be74a2
Piyush Taneja <tanejapiyush433@gmail.com>
2026-04-22 17:59:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ import torch
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 BATCH_SIZE = 128
-EPOCHS = 20
-LR = 0.001
+EPOCHS = 10
+LR = 0.0003
 
-# Try multiple lambda values
-LAMBDA_VALUES = [1e-5, 1e-4, 1e-3]
+# tuned lambdas
+LAMBDA_VALUES = [1e-3, 1e-2, 5e-2]
 
-THRESHOLD = 1e-2
+THRESHOLD = 0.1
